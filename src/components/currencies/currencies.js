449fd4b344d00a82814bd3fddd6a30a6eccf4464
Danil <danil@noreplay.noreplay.com>
2021-03-18 23:08:10 +0200

    display currencies

--- src/components/currencies/currencies.js
+++ src/components/currencies/currencies.js
@@ -1,34 +1,88 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import Header from '../header/header';
 import CoindeskApiService from '../../services/api-services';
 
 const CurrencyPage = () => {
 
-  const [list, setList] = useState([]);
+  // TODO: need add isLoaded for case whene response from API too slow
 
-  // TODO: need add isLoaded
+  const [time, setTime] = useState([]);
+  const [bpi, setBpi] = useState([]);
+  const [apiError, setStatus] = useState(false);
+
+  const [sortNumber, setNumbersSort] = useState();
+
+  const apiErrorMsg = `Sorry, We don't have any data, please wait a few minutes, 
+  if still there is no data, please come back later`;
 
   useEffect(() => {
-    let mounted = true;
-    CoindeskApiService.getMainCurrentPrice()
-      .then(items => {
-        if (mounted) {
-          setList(items)
-        }
-      })
-    return () => mounted = false;
+    getCurrenciesData();
+    setInterval(() => {
+      getCurrenciesData();
+    }, 10000);
   }, []);
+
+  const getCurrenciesData = () => {
+    CoindeskApiService.getMainCurrenciesPrices()
+      .then(items => {
+        setTime(items.time.updated);
+        setBpi(items.bpi);
+        setStatus(false);
+      }).catch(
+        error => setStatus(true)
+      );
+  }
+
+  const getSortedData = () => {
+    let arr = Object.entries(bpi);
+    let arrCodeRate = [];
+    for (let index = 0; index < arr.length; index++) {
+      arrCodeRate.push({ 
+        id:index,
+        code:arr[index][1].code, 
+        rate:arr[index][1].rate_float 
+      });
+    }
+    console.log('--->', arrCodeRate);
+    return(
+      <tbody>
+        {arrCodeRate.map((item) => (
+          <tr key={item.id}>
+            <td>{item.code}</td>
+            <td>${item.rate}</td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  };
 
   return (
     <div>
       <Header />
+
       <div className="container">
-        <div>currencyPage</div>
         <div>
-          <h1>Rates</h1>
-          <ul>
-            {JSON.stringify(list)}
-          </ul>
+          <h4>This data was produced from the CoinDesk Bitcoin Price Index (USD).</h4>
+          <p>Last update: {time} (updated every 10 sec)</p>
+          <p className="text-info">{apiError ? apiErrorMsg : ''}</p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">
+                  Code
+                </th>
+                <th scope="col">
+                  Rate <button className="btn btn-link" onClick={() => { setNumbersSort() }}>
+                            <span>&#129031;</span><span>&#129029;</span>
+                        </button>
+                </th>
+              </tr>
+            </thead>
+            { getSortedData() }
+          </table>
+          <div>
+            <p className="text-left">Powered by <a href="https://www.coindesk.com/price/bitcoin">CoinDesk</a></p>
+          </div>
         </div>
       </div>
     </div>
--- src/services/api-services.js
+++ src/services/api-services.js
@@ -6,11 +6,14 @@
       historical data
       https://api.coindesk.com/v1/bpi/historical/close.json
       https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05
+
+      TODO:
+      add something against many concurrent async requests
   */
 
   const apiBase = 'https://api.coindesk.com/v1/bpi/';
 
-  const getResource = async (url) => {
+  const getData = async (url) => {
     const res = await fetch(`${apiBase}${url}`);
 
     if (!res.ok) {
@@ -20,11 +23,23 @@
     return await res.json();
   };
 
-  const getMainCurrentPrice = async () => {
-    const res = await getResource(`currentprice.json`);
+  const getMainCurrenciesPrices = async () => {
+    const res = await getData(`currentprice.json`);
+    return res;
+  };
+
+  const getCurrencyPrice = async (CODE = 'CNY') => {
+    const res = await getData(`currentprice/${CODE}.json`);
+    return res;
+  };
+
+  const getHistoricalData = async () => {
+    const res = await getData(`historical/close.json`);
     return res;
   };
 
   export default {
-    getMainCurrentPrice
+    getMainCurrenciesPrices,
+    getCurrencyPrice,
+    getHistoricalData
   };
